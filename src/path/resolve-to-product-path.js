5e21db0fb978070ae78c2b73556901784230705c
Vladimir Belov <>
2019-12-19 14:38:25 +0200

    Added path/resolveToProductPath function

---
+++ src/path/resolve-to-product-path.js
@@ -0,0 +1,29 @@
+// @flow
+import parseExtensionPath from './parse-extension-path';
+import parseComponentTemplatePath from './parse-component-template-path';
+import parseSiteTemplatePath from './parse-site-template-path';
+
+export default function resolveToProductPath(sourcePath: string): ?string {
+	const parsedExtensionPath = parseExtensionPath(sourcePath);
+	if (parsedExtensionPath)
+	{
+		const {root, jsDir, extension, filePath} = parsedExtensionPath;
+		return `/${root}/js/${jsDir}/${extension.join('/')}/${filePath}`;
+	}
+
+	const parsedComponentPath = parseComponentTemplatePath(sourcePath);
+	if (parsedComponentPath)
+	{
+		const {root, namespace, component, template, filePath} = parsedComponentPath;
+		return `/${root}/components/${namespace}/${component}/templates/${template}/${filePath}`;
+	}
+
+	const parsedSiteTemplatePath = parseSiteTemplatePath(sourcePath);
+	if (parsedSiteTemplatePath)
+	{
+		const {root, template, filePath} = parsedSiteTemplatePath;
+		return `/${root}/templates/${template}/${filePath}`;
+	}
+
+	return null;
+}
